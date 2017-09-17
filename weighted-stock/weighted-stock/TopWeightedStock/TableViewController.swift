b2a678157a9394f00b449ed05bffd42c41d09f82
TimChen <timchen@kkbox.com>
2017-09-17 16:02:03 +0800

    Add WeightedStockList api to get response of html context

--- weighted-stock/weighted-stock/TopWeightedStock/TableViewController.swift
+++ weighted-stock/weighted-stock/TopWeightedStock/TableViewController.swift
@@ -14,6 +14,22 @@ class TableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        let api = WeightedStockListApi.init { (data, response, error) in
+            guard let data = data, error == nil else {                                                 // check for fundamental networking error
+                print("error=\(String(describing: error))")
+                return
+            }
+            
+            if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {           // check for http errors
+                print("statusCode should be 200, but is \(httpStatus.statusCode)")
+                print("response = \(String(describing: response))")
+            } else {
+                let responseStr = String(data: data, encoding: .utf8)!
+                print("response \(responseStr)")
+            }
+        }
+
+        api.start()
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
