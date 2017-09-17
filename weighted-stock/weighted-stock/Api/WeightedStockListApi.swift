30c12756478993409f47aab957e8e2a942c3a4b0
TimChen <timchen@kkbox.com>
2017-09-17 20:45:26 +0800

    Strict optional chaining

--- weighted-stock/weighted-stock/Api/WeightedStockListApi.swift
+++ weighted-stock/weighted-stock/Api/WeightedStockListApi.swift
@@ -1,13 +1,12 @@
 import Foundation
 
 class WeightedStockListApi {
-    var request : URLRequest
-    var task : URLSessionTask?
+    private var request : URLRequest
+    private var sessionTask : URLSessionTask?
     
     var callback:(Data?, URLResponse?, Error?)->Void
     
     init (cb : @escaping (Data?, URLResponse?, Error?)->Void) {
-        //let url = URL(string: "http://jsinfo2.wls.com.tw/z/zm/zmd/zmdb.djhtm")!
         let url = URL(string: "http://www.taifex.com.tw/chinese/9/9_7_1.asp")!
         request = URLRequest.init(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 5)
         request.httpMethod = "Get"
@@ -16,12 +15,17 @@ class WeightedStockListApi {
     
     func start() {
         cancel()
-        
-        task = URLSession.shared.dataTask(with: request, completionHandler:callback)
-        task?.resume()
+        sessionTask = URLSession.shared.dataTask(with: request, completionHandler:callback)
+
+        if let task = sessionTask {
+            task.resume()
+        }
     }
     
     func cancel() {
-        task?.cancel()
+        
+        if let task = sessionTask {
+            task.cancel()
+        }
     }
 }
