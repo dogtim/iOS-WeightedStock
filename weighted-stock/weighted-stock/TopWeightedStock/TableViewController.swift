4adb0a946108c07ab790b6d6b740b84623b60ea1
TimChen <timchen@kkbox.com>
2017-09-17 15:01:01 +0800

    Add submodule of Kanna (an XML/HTML parser for Swift) 1. https://github.com/tid-kijyun/Kanna 2. In the project settings add $(SDKROOT)/usr/include/libxml2 to the "header search paths" field

--- weighted-stock/weighted-stock/TopWeightedStock/TableViewController.swift
+++ weighted-stock/weighted-stock/TopWeightedStock/TableViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Kanna
 
 class TableViewController: UITableViewController {
         
@@ -39,6 +40,20 @@ class TableViewController: UITableViewController {
 
     @IBAction func backTouchUp(_ sender: Any) {
         dismiss(animated: true, completion: nil)
+    }
+    
+    func toTestKanna() {
+        let html = "<html><head></head><body><ul><li><input type='image' name='input1' value='string1value' class='abc' /></li><li><input type='image' name='input2' value='string2value' class='def' /></li></ul><span class='spantext'><b>Hello World 1</b></span><span class='spantext'><b>Hello World 2</b></span><a href='example.com'>example(English)</a><a href='example.co.jp'>example(JP)</a></body>"
+        
+        if let doc = HTML(html: html, encoding: .utf8) {
+            print(doc.title)
+            
+            // Search for nodes by XPath
+            for link in doc.xpath("//a | //link") {
+                print(link.text)
+                print(link["href"])
+            }
+        }
     }
     /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
