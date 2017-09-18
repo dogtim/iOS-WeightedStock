96f478c5beb8d955b05021e1e8cb9e21d028c1ec
TimChen <timchen@kkbox.com>
2017-09-18 22:24:13 +0800

    Show company name on table view

--- weighted-stock/weighted-stock/Stock/Stock.swift
+++ weighted-stock/weighted-stock/Stock/Stock.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 class Stock {
-    var rank : String = "0"
+    var rank = Int(0)
     var number : String = "0"
     var name : String = "0"
     var weighted : String = "0"
--- weighted-stock/weighted-stock/TopWeightedStock/TableViewController.swift
+++ weighted-stock/weighted-stock/TopWeightedStock/TableViewController.swift
@@ -10,12 +10,15 @@ import UIKit
 import Kanna
 
 class TableViewController: UITableViewController {
-
-    var stocks = [Stock]()
     
+    var stocks = [Stock]()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+    }
 
+    override func viewWillAppear(_ animated: Bool) {
         let api = WeightedStockListApi.init { (data, response, error) in
             guard let data = data, error == nil else {                                                 // check for fundamental networking error
                 print("error=\(String(describing: error))")
@@ -30,44 +33,55 @@ class TableViewController: UITableViewController {
                 //print("response \(responseStr)")
                 
                 if let doc = HTML(html: responseStr, encoding: .utf8) {
-
+                    
                     for (index, repo) in doc.xpath("//tr[@valign='bottom']").enumerated() {
-                        //print("index \(index) , text : \()")
                         self.toParseInternal(repo.toHTML!)
-                        self.showStocks()
-                        if(index == 30) {
+                        if(index == 29) {
                             break
                         }
                     }
+                    self.showStocks()
+                    DispatchQueue.main.async {
+                        self.tableView.reloadData()
+                    }
+                    
                 }
             }
         }
-
+        
         api.start()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return stocks.count
     }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as UITableViewCell
+        
+        if let myLabel = cell.textLabel {
+            myLabel.text =
+            "\(stocks[indexPath.row].name)"
+        }
+        
+        cell.textLabel?.numberOfLines = 0
+        cell.textLabel?.lineBreakMode = NSLineBreakMode.byCharWrapping
+        return cell
+    }
+    
     @IBAction func backTouchUp(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
     
     func showStocks() {
+        print("stock count \(stocks.count)")
         for stock in stocks {
             print(stock.name)
         }
@@ -82,20 +96,17 @@ class TableViewController: UITableViewController {
                 
                 switch index % 4 {
                 case 0:
-                    
-                    stock.rank = element.text!
+                    stock.rank = Int(element.text!)!
                 case 1:
-                    print("股號 \(element.text!)")
                     stock.number = element.text!
                 case 2:
-                    print("公司名 \(element.text!)")
                     stock.name = element.text!
                 case 3:
-                    print("佔比 \(element.text!)")
                     stock.weighted = element.text!
-                    stocks.insert(stock, at: (index / 4))
+                    if(stock.rank <= 30) {
+                        stocks.append(stock)
+                    }
                     stock = Stock()
-                    
                 default:
                     print("Unknow error")
                 }
@@ -104,58 +115,58 @@ class TableViewController: UITableViewController {
     }
     
     /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
+     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+     let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+     
+     // Configure the cell...
+     
+     return cell
+     }
+     */
+    
     /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
+     // Override to support conditional editing of the table view.
+     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the specified item to be editable.
+     return true
+     }
+     */
+    
     /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
+     // Override to support editing the table view.
+     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+     if editingStyle == .delete {
+     // Delete the row from the data source
+     tableView.deleteRows(at: [indexPath], with: .fade)
+     } else if editingStyle == .insert {
+     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+     }
+     }
+     */
+    
     /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
+     // Override to support rearranging the table view.
+     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+     
+     }
+     */
+    
     /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
+     // Override to support conditional rearranging of the table view.
+     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the item to be re-orderable.
+     return true
+     }
+     */
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
     
 }
