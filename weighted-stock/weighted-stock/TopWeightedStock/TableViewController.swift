053e8d926e7a6760abbfd2a52814b758aa37f107
TimChen <timchen@kkbox.com>
2017-09-18 22:04:30 +0800

    Remove testing codes

--- weighted-stock/weighted-stock/TopWeightedStock/TableViewController.swift
+++ weighted-stock/weighted-stock/TopWeightedStock/TableViewController.swift
@@ -35,7 +35,7 @@ class TableViewController: UITableViewController {
                         //print("index \(index) , text : \()")
                         self.toParseInternal(repo.toHTML!)
                         self.showStocks()
-                        if(index == 1) {
+                        if(index == 30) {
                             break
                         }
                     }
@@ -64,7 +64,6 @@ class TableViewController: UITableViewController {
     }
 
     @IBAction func backTouchUp(_ sender: Any) {
-        toTestKanna()
         dismiss(animated: true, completion: nil)
     }
     
@@ -80,11 +79,10 @@ class TableViewController: UITableViewController {
             
             // Search for nodes by XPath
             for (index, element) in doc.xpath("//td").enumerated() {
-                print("index \(index)")
                 
                 switch index % 4 {
                 case 0:
-                    print("排名 \(element.text!)")
+                    
                     stock.rank = element.text!
                 case 1:
                     print("股號 \(element.text!)")
@@ -105,22 +103,6 @@ class TableViewController: UITableViewController {
         }
     }
     
-    func toParseHtml(_ html : String) {
-        if let doc = HTML(html: html, encoding: .utf8) {
-            
-            // Search for nodes by XPath
-            for link in doc.xpath("//a | //link") {
-                print(link.text)
-                print(link["href"])
-            }
-        }
-    }
-    
-    func toTestKanna() {
-        let html = "<html><head></head><body><ul><li><input type='image' name='input1' value='string1value' class='abc' /></li><li><input type='image' name='input2' value='string2value' class='def' /></li></ul><span class='spantext'><b>Hello World 1</b></span><span class='spantext'><b>Hello World 2</b></span><a href='example.com'>example(English)</a><a href='example.co.jp'>example(JP)</a></body>"
-        
-        toParseHtml(html)
-    }
     /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
